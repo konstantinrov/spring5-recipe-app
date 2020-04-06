95705e94ffc121bd639118e6343190736b571c3e
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-06 13:26:40 +0200

    Adding One To One Unidirectional JPA relationships for UnitOfMeasure and Ingredient entites.

--- src/main/java/gkdev/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/gkdev/springframework/spring5recipeapp/domain/Ingredient.java
@@ -1,10 +1,12 @@
 package gkdev.springframework.spring5recipeapp.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import java.math.BigDecimal;
 
 /**
@@ -22,7 +24,8 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -57,6 +60,14 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
 
