6555a9750ce5d386ca47638572b9d16609dfc167
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-06 12:24:49 +0200

    Adding One To One JPA relationships for Recipe and Notes entites.

---
+++ src/main/java/gkdev/springframework/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,51 @@
+package gkdev.springframework.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+/**
+ * @author Konstantin Romanov
+ * @created 06/04/2020 - 12:10
+ * @project spring5-recipe-app
+ */
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
