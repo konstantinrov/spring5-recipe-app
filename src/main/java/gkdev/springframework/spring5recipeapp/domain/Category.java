90a94c8fb2ce419cf97ecc4007f526d843c90f28
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-06 13:50:42 +0200

    Adding Many To Many JPA relationships for Recipe and Category entites.

---
+++ src/main/java/gkdev/springframework/spring5recipeapp/domain/Category.java
@@ -0,0 +1,50 @@
+package gkdev.springframework.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+/**
+ * @author Konstantin Romanov
+ * @created 06/04/2020 - 13:35
+ * @project spring5-recipe-app
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories") //name of the attribute, other side.
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
