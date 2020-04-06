5cdf231ba4760c314399ef8e9113fa801b159342
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-06 15:41:56 +0200

    Refactoring Recipe Bootstrap.

--- src/main/java/gkdev/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/gkdev/springframework/spring5recipeapp/domain/Ingredient.java
@@ -33,10 +33,14 @@ public class Ingredient {
     public Ingredient() {
     }
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this(description, amount, uom);
         this.recipe = recipe;
     }
 
--- src/main/java/gkdev/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/gkdev/springframework/spring5recipeapp/domain/Recipe.java
@@ -142,6 +142,12 @@ public class Recipe {
         return ingredients;
     }
 
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
     }
