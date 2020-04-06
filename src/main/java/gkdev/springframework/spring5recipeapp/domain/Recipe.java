cc9cb86e04b5c69bf222a6affaf55fde328963bc
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-06 13:15:42 +0200

    Adding One To Many JPA relationships for Recipe and Ingredient entites.

--- src/main/java/gkdev/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/gkdev/springframework/spring5recipeapp/domain/Recipe.java
@@ -6,7 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.Set;
 
 /**
  * @author Konstantin Romanov
@@ -29,6 +31,9 @@ public class Recipe {
 
     //todo add
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -114,5 +119,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
