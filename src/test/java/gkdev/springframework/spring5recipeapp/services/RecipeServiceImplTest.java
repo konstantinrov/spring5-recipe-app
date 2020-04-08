50761d09ce24f7d577d30f4b8161feab503d9274
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-08 10:57:08 +0200

    Adding a Mockito Mock.

---
+++ src/test/java/gkdev/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,47 @@
+package gkdev.springframework.spring5recipeapp.services;
+
+import gkdev.springframework.spring5recipeapp.domain.Recipe;
+import gkdev.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Konstantin Romanov
+ * @created 08/04/2020 - 10:37
+ * @project spring5-recipe-app
+ */
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipeData = new HashSet();
+        recipeData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeData);
+
+        Set<Recipe> recipeSet = recipeService.getRecipes();
+
+        assertEquals(recipeSet.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
