1e129528e5ea4d538d8aae4df89c14890ff3064c
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-08 11:11:01 +0200

    Adding Mockito Mock for IndexController

---
+++ src/test/java/gkdev/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,41 @@
+package gkdev.springframework.spring5recipeapp.controllers;
+
+import gkdev.springframework.spring5recipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Konstantin Romanov
+ * @created 08/04/2020 - 10:59
+ * @project spring5-recipe-app
+ */
+public class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        String viewName = indexController.getIndexPage(model);
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
