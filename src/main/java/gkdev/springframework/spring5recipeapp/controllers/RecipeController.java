81c8fdbab303fb2a256ad88471cf6c3b5d95a042
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-16 15:01:24 +0200

    Creating a new recipe.

--- src/main/java/gkdev/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/gkdev/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package gkdev.springframework.spring5recipeapp.controllers;
 
+import gkdev.springframework.spring5recipeapp.commands.RecipeCommand;
 import gkdev.springframework.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,5 +28,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommad = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommad.getId();
     }
 }
