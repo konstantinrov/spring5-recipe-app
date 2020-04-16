d8bd7986abc39553ab54d36f62afef91acf7a2e3
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-16 13:41:12 +0200

    Displaying remaining recipe properties.

--- src/main/java/gkdev/springframework/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/gkdev/springframework/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -153,6 +153,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
+        guacRecipe.setUrl("https://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        guacRecipe.setServings(4);
+        guacRecipe.setSource("Simply Recipes");
 
         //add to return list
         recipes.add(guacRecipe);
