a1d0413bc9ad8074dd66a4130788eafc12b309ef
Konstantin Romanov <konstantin.romanov@live.be>
2020-04-28 17:39:58 +0200

    Bug fixing: ingredient controller

--- src/main/java/gkdev/springframework/spring5recipeapp/controllers/IngredientController.java
+++ src/main/java/gkdev/springframework/spring5recipeapp/controllers/IngredientController.java
@@ -2,7 +2,7 @@ package gkdev.springframework.spring5recipeapp.controllers;
 
 import gkdev.springframework.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @project spring5-recipe-app
  */
 @Slf4j
-@Service
+@Controller
 public class IngredientController {
 
     private final RecipeService recipeService;
