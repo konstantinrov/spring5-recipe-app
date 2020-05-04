a8512eec25d2612c15dfbec2664559239cfa36e2
Konstantin Romanov <konstantin.romanov@live.be>
2020-05-04 17:06:17 +0200

    Refactoring: adding a global Controller Exception Handler.

---
+++ src/main/java/gkdev/springframework/spring5recipeapp/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package gkdev.springframework.spring5recipeapp.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Konstantin Romanov
+ * @created 04/05/2020 - 17:00
+ * @project spring5-recipe-app
+ */
+@Slf4j
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception) {
+        log.error("Handling number format exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+    }
+}
