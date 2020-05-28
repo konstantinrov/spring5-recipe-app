2caffc77803985cd3167c7daa9f0e874f8f15f48
Konstantin Romanov <konstantin.romanov@live.be>
2020-05-28 14:07:00 +0200

    Adding schema generation with Hibernate & create tables scripts.

--- src/main/java/gkdev/springframework/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/gkdev/springframework/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -11,6 +11,7 @@ import gkdev.springframework.spring5recipeapp.repositories.RecipeRepository;
 import gkdev.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +28,7 @@ import java.util.Optional;
  */
 @Component
 @Slf4j
+@Profile("default")
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
